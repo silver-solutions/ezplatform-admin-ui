e09c92bec12b78b9cec53a2c499092f44358593c
Maciej Kobus <maciej.kobus@ez.no>
2020-06-10 16:20:17 +0200

    EZC-88: Created initial package structure

---
+++ .github/pull_request_template.md
@@ -0,0 +1,11 @@
+> JIRA: [EC-XXX](https://jira.ez.no/browse/EC-XXX)
+
+### Description 
+
+<!-- What problem are you trying to solve? How did you solve the problem? -->
+
+## Checklist
+
+- [ ] Code follows the code style of this project (use `$ composer fix-cs`).
+- [ ] Change requires a change to the documentation.
+- [ ] Code is ready for a review.
---
+++ src/bundle/IbexaCommerceAdminUiBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Commerce\Bundle\AdminUiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class IbexaCommerceAdminUiBundle extends Bundle
+{
+}

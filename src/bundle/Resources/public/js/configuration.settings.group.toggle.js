154b95be820405645664e6674bef2aea42b483fd
Michał Grabowski <michal.grabowski@ez.no>
2020-07-06 12:44:39 +0200

    EC-101: fixed bug in expanding configuration settings (#13)

--- src/bundle/Resources/public/js/configuration.settings.group.toggle.js
+++ src/bundle/Resources/public/js/configuration.settings.group.toggle.js
@@ -6,11 +6,15 @@
         const group = event.currentTarget.closest('.ez-card--commerce-configuration-group');
 
         if (group.classList.contains('ez-card--collapsed')) {
-            doc.querySelector('.ez-card--commerce-configuration-group:not(.ez-card--collapsed)').classList.add('ez-card--collapsed');
-            group.classList.remove('ez-card--collapsed');
-        } else {
-            group.classList.add('ez-card--collapsed');
+            const activePanel = group.closest('.tab-pane');
+            const notCollapsedGroup = activePanel.querySelector('.ez-card--commerce-configuration-group:not(.ez-card--collapsed)');
+
+            if (notCollapsedGroup) {
+                notCollapsedGroup.classList.add('ez-card--collapsed');
+            }
         }
+
+        group.classList.toggle('ez-card--collapsed');
     };
 
     togglers.forEach((button) => button.addEventListener('click', toggleFieldTypeView, false));
